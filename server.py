1644481491ab896be3ba74e1599698a0f36caac2
sirajmuneer123 <sirajmuneer4@gmail.com>
2015-11-17 11:37:01 +0530

    n client chat

--- server.py
+++ server.py
@@ -7,11 +7,10 @@ HOST="127.0.0.1"
 PORT=5003
 DATA_BUFFER=1024
 SOCKET_LIST=[]
-BROADCAST_LIST=[]
 
-def broadcast_data(sock,message):
-	for socket in BROADCAST_LIST:
-		if socket != sock:
+def broadcast_data(sock,message,server_socket):
+	for socket in SOCKET_LIST:
+		if socket != sock and socket != server_socket and socket != sys.stdin:
 			socket.send(message)
 
 
@@ -22,6 +21,7 @@ def server():
 	server_socket.listen(5)
 	print 'Waiting for clients..........'
 	SOCKET_LIST.append(server_socket)
+	SOCKET_LIST.append(sys.stdin)
 	
 
 	while 1:
@@ -30,11 +30,13 @@ def server():
 			if sock == server_socket:
 				sock_fd,addr=server_socket.accept()
 				SOCKET_LIST.append(sock_fd)
-				BROADCAST_LIST.append(sock_fd)
+			elif sock == sys.stdin:
+				message=raw_input()
+				broadcast_data('',"\r" + '<message from server> ' + message,server_socket)
 			else:
 				data=sock.recv(DATA_BUFFER)
 				print '<',str(sock.getpeername()),'> ',data
-				broadcast_data(sock,"\r" + '<' + str(sock.getpeername()) + '> ' + data)
+				broadcast_data(sock,"\r" + '<' + str(sock.getpeername()) + '> ' + data,server_socket)
 	sock.close()
 			
 			
